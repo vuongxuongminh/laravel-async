b14751c7c6111b42608fa56b3668d4322f28dafd
Vuong Minh <vuongxuongminh@gmail.com>
2019-06-12 11:31:38 +0700

    Extends synchronous process for hotfix

---
+++ src/Process/SynchronousProcess.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @link https://github.com/vuongxuongminh/laravel-async
+ *
+ * @copyright (c) Vuong Xuong Minh
+ * @license [MIT](https://opensource.org/licenses/MIT)
+ */
+
+namespace VXM\Async\Process;
+
+use Throwable;
+use Spatie\Async\Process\SynchronousProcess as BaseSynchronousProcess;
+
+/**
+ * @author Vuong Minh <vuongxuongminh@gmail.com>
+ * @since 1.0.0
+ */
+class SynchronousProcess extends BaseSynchronousProcess
+{
+    /**
+     * Hotfix: https://github.com/spatie/async/pull/73
+     *
+     * @return Throwable
+     */
+    protected function resolveErrorOutput(): Throwable
+    {
+        return $this->getErrorOutput();
+    }
+}
