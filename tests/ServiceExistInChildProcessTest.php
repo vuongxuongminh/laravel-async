2ce341441193f5584a6e1d1ef77960296d994ea6
Vuong Minh <vuongxuongminh@gmail.com>
2019-06-12 16:31:02 +0700

    Added services exist test

---
+++ tests/ServiceExistInChildProcessTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @link https://github.com/vuongxuongminh/laravel-async
+ *
+ * @copyright (c) Vuong Xuong Minh
+ * @license [MIT](https://opensource.org/licenses/MIT)
+ */
+
+namespace VXM\Async\Tests;
+
+use Async;
+
+/**
+ * @author Vuong Minh <vuongxuongminh@gmail.com>
+ * @since 1.0.0
+ */
+class ServiceExistInChildProcessTest extends TestCase
+{
+    public function testBaseServicesExist()
+    {
+        Async::run(function () {
+            return app()->has('events') && app()->has('log') && app()->has('router');
+        });
+
+        $this->assertTrue(current(Async::wait()));
+    }
+}
