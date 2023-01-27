a0383cda7a5d89631d20d7a40bb2c4464334f1a7
Minh Vuong <vuongxuongminh@gmail.com>
2023-01-27 10:49:54 +0700

    tests: add async test

---
+++ tests/AsyncTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @link https://github.com/vuongxuongminh/laravel-async
+ *
+ * @copyright (c) Vuong Xuong Minh
+ * @license [MIT](https://opensource.org/licenses/MIT)
+ */
+
+namespace VXM\Async\Tests;
+
+use Async;
+
+use VXM\Async\Pool;
+
+class AsyncTest extends TestCase
+{
+    public function testAsync()
+    {
+        /** @var Pool $pool */
+        $pool = app('async.pool');
+
+        for ($i = 0; $i < 10; ++$i) {
+            Async::run(function () use ($i) {
+                sleep(1);
+
+                return $i;
+            });
+        }
+
+        $current = time();
+        $results = Async::wait();
+
+        if (Pool::isSupported()) {
+            $this->assertEquals(1, time() - $current);
+        } else {
+            $this->assertEquals(10, time() - $current);
+        }
+
+        sort($results);
+
+        $this->assertEquals(range(0, 9), $results);
+    }
+}
